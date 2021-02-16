a347a05587aa90ebb6be1926df0ac575fceec65b
Jude <jantungkuno@gmail.com>
2021-02-16 17:42:28 -0500

    updating orm for crud

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,5 @@
 const connection = require('./connection.js');
 
-const orm = {
-    all: function(tableInput, cb){
-        connection.query('SELECT * FROM ' + tableInput + ';', function(err,result){
-            if(err) throw err;
-            cb(result)
-        })
-    },
 
-    update: function(tableInput, condition, cb){
-        connection.query('UPDATE' +tableInput+ 'SET devoured=true WHERE id='+condition+ ';', function(err,result){
-            if(err) throw err;
-            cb(result);
-        })
-    }
-}
  
 module.exports = orm;
