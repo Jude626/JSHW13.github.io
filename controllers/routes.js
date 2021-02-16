ec21f113c1cb8b16d003e849a785cbc80c4df08c
Jude <jantungkuno@gmail.com>
2021-02-16 16:27:03 -0500

    update module exports to routes js

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const burger = require('../models/burger.js');
 
 router.get('/', function(req,res){
+    burger.all(function(burger_data){
+        console.log(burger_data);
+  
     res.render('index');
+    }) 
 })
 
 module.exports = router;
