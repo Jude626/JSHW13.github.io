9085554dfca33ed4f1f9cca6f033359ea0d752e6
Jude <jantungkuno@gmail.com>
2021-02-16 15:44:14 -0500

    created global variables

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
