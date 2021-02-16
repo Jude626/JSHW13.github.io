4b741937a3535fdab76812535ec6589a8922de33
Jude <jantungkuno@gmail.com>
2021-02-16 15:45:34 -0500

    created app var

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
+const app = express();
+app.use(express.static(_dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use
