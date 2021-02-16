86943afe0b284801b5206f1cdcb7ba22782f9edd
Jude <jantungkuno@gmail.com>
2021-02-16 16:50:13 -0500

    starting update function

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'ENTERLATER!!!',
+    password:'Kay626#',
     database:'burgers_db'
 })
 
