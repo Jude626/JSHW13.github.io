76b955938417387e7ee2db565bb2039dfdfdc6cd
Jude <jantungkuno@gmail.com>
2021-03-28 19:32:05 -0400

    added post to route.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -8,7 +8,14 @@ router.get('/', function(req,res){
   
     res.render('index',{burger_data});
     }) 
-})
+});
+
+router.post('/burgers/create', function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect('/');
+    });
+});
 
 router.put('/burgers/update',function(req,res){
     burger.update(req.body.burger_id, function(result){
